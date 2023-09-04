e8006a645c70f36f174a38a3b94c166a3b91eb1d
Mantey Caleb <manteycaleb@gmail.com>
2023-09-04 00:33:40 +0000

    added the editor tools script

--- Editor/ToolsMenu.cs
+++ Editor/ToolsMenu.cs
@@ -21,62 +21,74 @@ namespace Relu.Tools
         }
 
         #region XRTools
-            [MenuItem("Relu/XR/Basic Setup")]
-            public static void SetupXRPackages()
+        [MenuItem("Relu/XR/Basic Setup")]
+        public static void SetupXRPackages()
+        {
+            PackagesSetup.SetupXRPackages();
+        }
+
+        [MenuItem("Relu/XR/XR Interaction Toolkit/Setup")]
+        public static void AddXRInteractionToolkit()
+        {
+            PackagesSetup.InstallPackage("xr.interaction.toolkit");
+        }
+
+        [MenuItem("Relu/XR/XR Interaction Toolkit/Install Samples")]
+        public static void OpenPackage()
+        {
+            UnityEditor.PackageManager.Requests.ListRequest listRequest = UnityEditor.PackageManager.Client.List();
+            while (!listRequest.IsCompleted)
             {
-                PackagesSetup.SetupXRPackages();
+                // Wait for the PackageManager to be ready
             }
 
-            [MenuItem("Relu/XR/XR Interaction Toolkit/Setup")]
-            public static void AddXRInteractionToolkit()
+            // Optionally, you can search for a specific package after opening the PackageManager window
+            string packageNameToSearch = "com.xr.interaction.toolkit";
+            UnityEditor.PackageManager.Requests.SearchRequest searchRequest = UnityEditor.PackageManager.Client.Search(packageNameToSearch);
+            while (!searchRequest.IsCompleted)
             {
-                PackagesSetup.InstallPackage("xr.interaction.toolkit");
+                // Wait for the search to complete
             }
 
-            [MenuItem("Relu/XR/XR Interaction Toolkit/Install Samples")]
-            public static void OpenPackage()
-            {
-                UnityEditor.PackageManager.Requests.ListRequest listRequest = UnityEditor.PackageManager.Client.List();
-                while (!listRequest.IsCompleted)
-                {
-                    // Wait for the PackageManager to be ready
-                }
-
-                // Optionally, you can search for a specific package after opening the PackageManager window
-                string packageNameToSearch = "com.xr.interaction.toolkit";
-                UnityEditor.PackageManager.Requests.SearchRequest searchRequest = UnityEditor.PackageManager.Client.Search(packageNameToSearch);
-                while (!searchRequest.IsCompleted)
-                {
-                    // Wait for the search to complete
-                }
-
-            }
+        }
         #endregion
-        
+
         #region CodeGeneration
-            [MenuItem("Relu/Create/Code/Monobehaviour")]
-            public static void CreateMonoBehaviourScript()
-            {
-                CodeGeneration.CreateScript("Monobehaviour.cs.txt", "NewScript.cs");
-            }
+        [MenuItem("Relu/Create/Code/Monobehaviour")]
+        public static void CreateMonoBehaviourScript()
+        {
+            CodeGeneration.CreateScript("Monobehaviour.cs.txt", "NewScript.cs");
+        }
 
-            [MenuItem("Relu/Create/Code/ScriptableObject")]
-            public static void CreateScriptableObject()
-            {
-                CodeGeneration.CreateScript("ScriptableObjects.cs.txt", "NewScriptableObject.cs");
-            }
+        [MenuItem("Relu/Create/Code/ScriptableObject")]
+        public static void CreateScriptableObject()
+        {
+            CodeGeneration.CreateScript("ScriptableObjects.cs.txt", "NewScriptableObject.cs");
+        }
 
-            [MenuItem("Relu/Create/Code/Interface")]
-            public static void CreateInterface()
-            {
-                CodeGeneration.CreateScript("Interface.cs.txt", "NewInterface.cs");
-            }
+        [MenuItem("Relu/Create/Code/Interface")]
+        public static void CreateInterface()
+        {
+            CodeGeneration.CreateScript("Interface.cs.txt", "NewInterface.cs");
+        }
 
-            [MenuItem("Relu/Create/Code/Abstract Class")]
-            public static void CreateAbstractClass()
-            {
-                CodeGeneration.CreateScript("AbstractClass.cs.txt", "NewAbstractClass.cs");
-            }
+        [MenuItem("Relu/Create/Code/Abstract Class")]
+        public static void CreateAbstractClass()
+        {
+            CodeGeneration.CreateScript("AbstractClass.cs.txt", "NewAbstractClass.cs");
+        }
+
+        [MenuItem("Relu/Create/Code/Editor/Editor Window")]
+        public static void CreateEditorWindowScript()
+        {
+            CodeGeneration.CreateScript("EditorWindow.cs.txt", "NewEditorWindow.cs");
+        }
+
+        [MenuItem("Relu/Create/Code/Editor/Editor Script")]
+        public static void CreateEditorScript()
+        {
+            CodeGeneration.CreateScript("Editor.cs.txt", "NewEditor.cs");
+        }
         #endregion
     }
 }
